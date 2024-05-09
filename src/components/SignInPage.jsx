800ce9c7aae2dea09c39b8d3b9ecc2fbc3679b90
devansh0331 <devanshshrivastava07@gmail.com>
2024-05-09 15:40:06 +0530

    back to home added in signin & signup

--- src/components/SignInPage.jsx
+++ src/components/SignInPage.jsx
@@ -2,10 +2,22 @@ import React from "react";
 
 import pilot from "../assets/drone.png";
 import SignIn from "./SignIn";
+import { useNavigate } from "react-router-dom";
 
 function SignInPage() {
+  const navigate = useNavigate();
+  const handleNavigate = (e) => {
+    preventDefault();
+    navigate("/");
+  };
   return (
-    <header className="bg-gray-200 h-screen w-screen flex ">
+    <header className="relative bg-gray-200 h-screen w-screen flex ">
+      <button
+        onClick={handleNavigate}
+        className="absolute top-5 left-5 md:top-10 md:left-10 font-rubik border-black border-2 rounded-md px-2 py-1 hover:bg-black hover:text-white"
+      >
+        &larr; Back to Home
+      </button>
       <div className="hidden md:block  ml-40 flex-1.5">
         <img
           className="h-screen w-full  object-cover object-center"
--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -2,10 +2,22 @@ import React from "react";
 
 import pilot from "../assets/drone.png";
 import SignUp from "./SignUp";
+import { useNavigate } from "react-router-dom";
 
 function SignUpPage() {
+  const navigate = useNavigate();
+  const handleNavigate = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
   return (
-    <header className="bg-gray-200 h-screen w-screen flex  ">
+    <header className="relative bg-gray-200 h-screen w-screen flex  ">
+      <button
+        onClick={handleNavigate}
+        className="absolute top-5 left-5 md:top-10 md:left-10 font-rubik border-black border-2 rounded-md px-2 py-1 hover:bg-black hover:text-white"
+      >
+        &larr; Back to Home
+      </button>
       <div className="hidden md:block ml-40 flex-1.5">
         <img
           className="h-full w-full  object-cover object-center"
